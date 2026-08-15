0332ac0fb109646b0494eb89c98bab5b0e8ab77c
Nymoddo <nymoddo@gmail.com>
2026-08-15 01:40:22 -0600

    Create build.gradle.kts

---
+++ src/en/theyiffgallery/build.gradle.kts
@@ -0,0 +1,10 @@
+ext {
+    extName = "The Yiff Gallery"
+    extClass = ".TheyYiffGallery"
+    extVersionCode = 1
+    isNsfw = true // true si el sitio es +18
+}
+
+metadata {
+    description = "Extension for the webpage The Yiff Gallery"
+}
